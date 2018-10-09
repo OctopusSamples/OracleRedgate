3b1473f2bb41169879f93afe855f171b4c699e86
Bob.Walker <bob.walker@octopus.com>
2018-10-09 22:28:12 +0000

    Initial Commit

---
+++ db/src/SOURCEDB/Sequences/ARTICLETABLESEQUENCE.sql
@@ -0,0 +1,1 @@
+CREATE SEQUENCE sourcedb.articletablesequence MAXVALUE 2147568;
---
+++ db/src/SOURCEDB/Tables/ARTICLETABLE.sql
@@ -0,0 +1,4 @@
+CREATE TABLE sourcedb.articletable (
+  "ID" NUMBER(*,0) NOT NULL,
+  "DATA" VARCHAR2(512 BYTE) NOT NULL
+);
